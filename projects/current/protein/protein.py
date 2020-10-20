415bacc95898e6ec2325ddd963048aab37a31512
maggie <mag@spectre>
2020-10-20 16:48:17 -0400

    Added Entry objects which hold a list of [atom] Record objects.

--- projects/current/protein/protein.py
+++ projects/current/protein/protein.py
@@ -10,6 +10,7 @@ from lib.jsondata import *
 partial_url = "https://files.rcsb.org/view/"
 pdb_objects_list = []
 pdb_objects = {}
+pdb_entries = []
 total_pdb_output = ''
 cwd = os.getcwd()
 out_dir = cwd + SEP + 'output' + SEP
@@ -36,10 +37,8 @@ def pdb_stuff(url, path, pdb_id):
     urllib.request.urlretrieve(url, path)
     file = open(path, 'r')
     f_lines = file.readlines()
-    out_str = ''
     tmp_entry = new_entry(lines=f_lines)
-    print('tmp entry | PDB id: ' + tmp_entry.pdb_id + ' | header: ' + tmp_entry.header + ' | title: ' +
-          tmp_entry.title + ' | ec nums: ' + str(tmp_entry.ec_nums))
+
     for line in f_lines:
         if 'ATOM' in line or 'HETATM' in line:
             tmp_record = new_record(line, pdb_id)
@@ -47,6 +46,11 @@ def pdb_stuff(url, path, pdb_id):
                 total_pdb_output += tmp_record.important_str()
                 if pdb_id in pdb_objects.keys(): pdb_objects[pdb_id].append(tmp_record)
                 else: pdb_objects[pdb_id] = [tmp_record]
+            tmp_entry.records.append(tmp_record)
+    print('tmp entry | PDB id: ' + tmp_entry.pdb_id + ' | header: ' + tmp_entry.header + ' | title: ' +
+          tmp_entry.title + ' | ec nums: ' + str(tmp_entry.ec_nums) + ' | no. of (relevant) atom records: ' +
+          str(len(tmp_entry.records)))
+    pdb_entries.append(tmp_entry)
 
 
 #
