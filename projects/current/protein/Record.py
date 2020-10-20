3e56e9522afb379989d5ca0808c99b3c8137286e
maggie <mag@spectre>
2020-10-20 14:47:08 -0400

    Update 6DHL.pdb, Record.py, and 189 more files...

--- projects/current/protein/Record.py
+++ projects/current/protein/Record.py
@@ -1,6 +1,7 @@
 class Record:
-    def __init__(self, label, serial, atom_name, alt_loc, ligand_code, chain_id, ligand_seq, ins_code, pos_x, pos_y,
-                 pos_z, occupy, temp, elem, charge):
+    def __init__(self, pdb_id, label, serial, atom_name, alt_loc, ligand_code, chain_id, ligand_seq, ins_code, pos_x,
+                 pos_y, pos_z, occupy, temp, elem, charge):
+        self.pdb_id = pdb_id
         self.label = label
         self.serial = serial
         self.atom_name = atom_name
@@ -18,19 +19,20 @@ class Record:
         self.charge = charge
 
     def important_str(self):
-        out = '> ' + self.label + ' | AtomName: ' + self.atom_name + ' | Serial: ' + self.serial + ' | Elem: ' + \
-              self.elem + ' | Ligand: ' + self.ligand_code + ' | LigandSeq: ' + self.ligand_seq + ' | X,Y,Z: (' + \
-              self.pos_x + ', ' + self.pos_y + ', ' + self.pos_z + ') | ChainID: ' + self.chain_id + '\n'
+        out = '> ' + self.pdb_id + ', ' + self.label + ' | AtomName: ' + self.atom_name + ' | Serial: ' + self.serial \
+              + '| Elem: ' + self.elem + ' | Ligand: ' + self.ligand_code + ' | LigandSeq: ' + self.ligand_seq + \
+              ' | X,Y,Z: (' + self.pos_x + ', ' + self.pos_y + ', ' + self.pos_z + ') | ChainID: ' + self.chain_id + \
+              '\n'
         return out
 
 
-def new_record(line):
+def new_record(line, name):
     if len(line) < 60: return None
     else:
         # ranges taken from the PDB documentation, column values are found in record_format_atom_hetatm.txt
         # beginning of range listed in file is one less due to list indices starting at 0. the end value is the same
         # since taking a subsection of a string works as such: substring = string[start:end-1]
-        tmp_record = Record(label=line[0:6], serial=line[6:11], atom_name=line[12:16], alt_loc=line[16:17],
+        tmp_record = Record(pdb_id=name, label=line[0:6], serial=line[6:11], atom_name=line[12:16], alt_loc=line[16:17],
                             ligand_code=line[17:20], chain_id=line[21:22], ligand_seq=line[22:26], ins_code=line[26:27],
                             pos_x=line[30:38], pos_y=line[38:45], pos_z=line[46:54], occupy=line[54:60],
                             temp=line[60:66], elem=line[76:78], charge=line[78:80])
--- projects/current/protein/protein.py
+++ projects/current/protein/protein.py
@@ -1,30 +1,49 @@
 import os
 import sys
 import urllib.request
-from Record import *
+from pathlib import Path
 
 sys.path.append(os.getcwd().replace(os.sep + 'protein', ''))
+from Record import *
 from lib.jsondata import *
 
 partial_url = "https://files.rcsb.org/view/"
+pdb_objects_list = []
+pdb_objects = {}
+total_pdb_output = ''
+cwd = os.getcwd()
+out_dir = cwd + SEP + 'output' + SEP
+formatted_out = out_dir + 'format_out_all.txt'
+formatted_out_small = out_dir + 'format_out_partial.txt'
+pdb_dir = out_dir + 'pdb_files' + SEP
+
+Path(out_dir).mkdir(parents=True, exist_ok=True)
+Path(pdb_dir).mkdir(parents=True, exist_ok=True)
 
 
 def main():
-    tst_url = partial_url + '6DHL.pdb'
-    pdb_stuff(tst_url, '6DHL.pdb')
+    for pdb_id in pdb_id_list:
+        print('looking at: ' + str(pdb_id))
+        tst_url = partial_url + pdb_id + '.pdb'
+        pdb_stuff(tst_url, pdb_dir + pdb_id + '.pdb', pdb_id)
+    with open(formatted_out, 'w+') as out_file:
+        out_file.write(total_pdb_output)
+        out_file.close()
 
 
-def pdb_stuff(url, name):
-    urllib.request.urlretrieve(url, name)
-    file = open(name, 'r')
+def pdb_stuff(url, path, pdb_id):
+    global total_pdb_output
+    urllib.request.urlretrieve(url, path)
+    file = open(path, 'r')
     f_lines = file.readlines()
     out_str = ''
     for line in f_lines:
         if 'ATOM' in line or 'HETATM' in line:
-            tmp_record = new_record(line)
+            tmp_record = new_record(line, pdb_id)
             if tmp_record.ligand_code.strip() in ligand_codes:
-                out_str += tmp_record.important_str()
-    print(out_str)
+                total_pdb_output += tmp_record.important_str()
+                if pdb_id in pdb_objects.keys(): pdb_objects[pdb_id].append(tmp_record)
+                else: pdb_objects[pdb_id] = [tmp_record]
 
 
 #
