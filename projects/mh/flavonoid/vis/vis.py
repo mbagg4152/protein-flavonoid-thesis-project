6aded549eb23c5ff4455454afd91996fd4413460
Maggie <50422221+mbagg4152@users.noreply.github.com>
2021-12-14 16:40:19 -0500

    .

--- projects/mh/flavonoid/vis/vis.py
+++ projects/mh/flavonoid/vis/vis.py
@@ -11,7 +11,7 @@ import numpy as np
 import os
 import pandas as pd
 
-flav_ids = ['AGI', 'BUN', 'KXN', 'HWB', 'EC', 'EGT', 'ERD', 'GC', 'GEN', 'HCC', 'KMP', 'LU2', 'MYC', 'NAR', 'QUE']
+flav_ids = ['AGI', 'BUN', 'KXN', 'HWB', 'EKXN', 'EGT', 'ERD', 'GKXN', 'GEN', 'HCC', 'KMP', 'LU2', 'MYC', 'NAR', 'QUE']
 
 names = {'AGI': 'Apigenin', 'BUN': 'Butein', 'KXN': 'Catechin', 'HWB': 'Cyanidin', 'EC': 'Epicatechin',
          'EGT': 'Epigallocatechin', 'ERD': 'Eriodictyol', 'GC': 'Gallocatechin', 'GEN': 'Genistein',
@@ -35,41 +35,46 @@ def main():
 def mk_tables(df):
     try: os.mkdir('tables')
     except (FileExistsError, OSError) as e: pass
+    try: os.mkdir('csv-tables')
+    except (FileExistsError, OSError) as e: pass
     for flav in flav_ids:
-        kd = df.loc[df[flav] == 'kegg'][flav].index.values.tolist()
-        lk = df.loc[df[flav] == 'lit-kegg'][flav].index.values.tolist()
-        rk = df.loc[df[flav] == 'rel-kegg'][flav].index.values.tolist()
-        ld = df.loc[df[flav] == 'lit'][flav].index.values.tolist()
-        rel = df.loc[df[flav] == 'rel'][flav].index.values.tolist()
-        rkc = rk.copy()
+        x_kegg = df.loc[df[flav] == 'K'][flav].index.values.tolist()
+        x_lit_kegg = df.loc[df[flav] == 'LK'][flav].index.values.tolist()
+        x_rel_kegg = df.loc[df[flav] == 'SK'][flav].index.values.tolist()
+        x_lit = df.loc[df[flav] == 'L'][flav].index.values.tolist()
+        x_rel = df.loc[df[flav] == 'S'][flav].index.values.tolist()
 
-        konly = name_wrap(kd)
+        all_kegg = np.array_split(np.array(name_wrap(x_kegg + x_lit_kegg + x_rel_kegg)), 4)
+        all_lit = np.array_split(np.array(name_wrap(x_lit + x_lit_kegg)), 4)
+        all_rel = np.array_split(np.array(name_wrap(x_rel_kegg + x_rel)), 4)
+        excl_kegg = np.array_split(np.array(name_wrap(x_kegg)), 4)
+        excl_lit_kegg = np.array_split(np.array(name_wrap(x_lit_kegg)), 4)
+        excl_lit = np.array_split(np.array(name_wrap(x_lit)), 4)
+        excl_sim_kegg = np.array_split(np.array(x_rel_kegg), 4)
 
-        # for i in range(len(rk)): rk[i] = rk[i] + '**'
-        # for i in range(len(rk)): rkc[i] = rkc[i] + '**'
-        # for i in range(len(rel)): rel[i] = rel[i] + '**'
+        all_kegg_df = format_df(all_kegg, max_lst_len(all_kegg), flav)
+        all_lit_df = format_df(all_lit, max_lst_len(all_lit), flav)
+        all_sim_df = format_df(all_rel, max_lst_len(all_rel), flav)
+        excl_kegg_df = format_df(excl_kegg, max_lst_len(excl_kegg), flav)
+        excl_lit_df = format_df(excl_lit, max_lst_len(excl_lit), flav)
+        excl_lit_kegg_df = format_df(excl_lit_kegg, max_lst_len(excl_lit_kegg), flav)
+        excl_sim_kegg_df = format_df(excl_sim_kegg, max_lst_len(excl_sim_kegg), flav)
 
-        lonly = name_wrap(ld + rel)
-        lit = name_wrap(ld + lk)
+        output_table(all_kegg_df, 'tables/' + 'all_kegg_' + flav + ftype)  # all predicted entries
+        output_table(all_lit_df, 'tables/' + 'all_lit_' + flav + ftype)  # all entries with exact lit matches
+        output_table(all_sim_df, 'tables/' + 'all_sim_' + flav + ftype)  # all entries with sim lit matches (no exact)
+        output_table(excl_kegg_df, 'tables/' + 'just_kegg_' + flav + ftype)  # predicted with no matches
+        output_table(excl_lit_df, 'tables/' + 'just_lit_' + flav + ftype)  # exact match but no predictions
+        output_table(excl_lit_kegg_df, 'tables/' + 'just_lit_kegg_' + flav + ftype)  # exact match AND predicted
+        output_table(excl_sim_kegg_df, 'tables/' + 'just_sim_kegg_' + flav + ftype)  # sim match AND predicted
 
-        both = name_wrap(lk + rk)
-        # for i in range(len(lk)): lk[i] = lk[i] + '*'
-        kegg = name_wrap(kd + lk + rk)
-
-        konly = np.array_split(np.array(konly), 5)
-        lonly = np.array_split(np.array(lonly), 5)
-        both = np.array_split(np.array(both), 5)
-        rkegg = np.array_split(np.array(rk), 5)
-        kegg = np.array_split(np.array(kegg), 5)
-        lit = np.array_split(np.array(lit), 5)
-
-        output_table(format_df(kegg, max_lst_len(kegg), flav, 'k'), 'tables/' + 'all_kegg_' + flav + ftype)
-        output_table(format_df(konly, max_lst_len(konly), flav, 'ko'), 'tables/' + 'just_kegg_' + flav + ftype)
-
-        output_table(format_df(lonly, max_lst_len(lonly), flav, 'lo'), 'tables/' + 'just_lit_' + flav + ftype)
-        output_table(format_df(lit, max_lst_len(lit), flav, 'l'), 'tables/' + 'all_lit_' + flav + ftype)
-        output_table(format_df(both, max_lst_len(both), flav, 'b'), 'tables/' + 'just_lit_kegg_' + flav + ftype)
-        output_table(format_df(rkegg, max_lst_len(rkegg), flav, 'rk'), 'tables/' + 'just_rel_kegg_' + flav + ftype)
+        all_kegg_df.to_csv(index=False, header=False, path_or_buf='csv-tables/all_kegg_' + flav + '.csv')
+        all_lit_df.to_csv(index=False, header=False, path_or_buf='csv-tables/all_lit_' + flav + '.csv')
+        all_sim_df.to_csv(index=False, header=False, path_or_buf='csv-tables/all_sim_' + flav + '.csv')
+        excl_kegg_df.to_csv(index=False, header=False, path_or_buf='csv-tables/just_kegg_' + flav + '.csv')
+        excl_lit_df.to_csv(index=False, header=False, path_or_buf='csv-tables/just_lit_' + flav + '.csv')
+        excl_lit_kegg_df.to_csv(index=False, header=False, path_or_buf='csv-tables/just_lit_kegg_' + flav + '.csv')
+        excl_sim_kegg_df.to_csv(index=False, header=False, path_or_buf='csv-tables/just_sim_kegg_' + flav + '.csv')
 
 
 def name_wrap(lst):
@@ -82,9 +87,7 @@ def name_wrap(lst):
             wrd = lst[idx].split()
             sp = ''
             for i in range((2 * len(str(num))) + 3): sp += ' '
-            # lst[idx] = str(num) + '. ' + wrd[0] + ' ' + wrd[1] + '\n' + sp + \
-            #            ' '.join([x.lower() for x in wrd[2:]])
-            lst[idx] = str(num) + '. ' + wrd[0] + ' ' + wrd[1]
+            if num < 10: lst[idx] = str(num) + '. ' + wrd[0] + ' ' + wrd[1]
         else: lst[idx] = str(num) + '. ' + lst[idx]
         num += 1
     return lst
@@ -99,12 +102,23 @@ def max_lst_len(lst):
     else: return 1
 
 
+def tweak_for_csv(df):
+    col = df.columns
+    for i in range(0, df.shape[0]):
+        for j in range(0, len(col)):
+            sp = (len(str((j + 2)))) * ' '
+            try: df.at[i, col[j]] = df.at[i, col[j]].replace('. ', ".").replace(' ', '\n' + sp).replace('.', '. ')
+            except KeyError:
+                print(df)
+                print(str(i) + str(j))
+    return df
+
+
 def output_table(df, fname):
     if len(df):
         row_cnt = len(df.values.tolist())
         plt.clf()
         tab = plt.table(cellText=df.values, loc='center', cellLoc='left', colLoc='left', edges='open')
-        # tab.scale(1, 1.5)
         plt.axis('off')
         plt.axis('tight')
         tab.auto_set_font_size(False)
@@ -117,39 +131,21 @@ def output_table(df, fname):
             cell.PAD = 0.085
             cell.set_linewidth(0)
 
-        # tcells = tab.get_celld()
-        # for i in range(0, len(vals)):
-        #     for j in range(0, len(df.head())):
-        #         tcells[(i, j)].set_fontsize(6)
-        #         # if vals[i]:
-        #         #     tcells[(i, j)].set_height(0.048)
-        #         # else:
-        #         #     tcells[(i, j)].set_height(0.048)
-        #         tcells[(i, j)].set_text_props(linespacing=1, ha='left', wrap=False)
-        #         tcells[(i, j)].set_height(0.03)
-        #         tcells[(i, j)].PAD = 0.085
-        #         tcells[(i, j)].set_linewidth(0)
-
         for col in range(len(df.columns)): tab.auto_set_column_width(col)
         plt.savefig(fname, bbox_inches='tight', dpi=400, format='png', pad_inches=0)
 
 
-def format_df(lst, ml, comp, tag):
+def format_df(lst, ml, comp):
     df = pd.DataFrame({'del': np.arange(ml)})
     cnt = 1
     for b in lst:
         if isinstance(b, str): b = np.array([b])
         if len(b.tolist()) == 0: b = [' ']
         else: b = b.tolist() + mk_blank_list(ml - len(b.tolist()))
+
         df['C' + str(cnt)] = b
         cnt += 1
     df = df.drop(columns=['del'])
-    # df.loc[len(df.index)] = ['', '', '', '', '']
-
-    # if (tag != 'l') and (tag != 'lo'):
-    #     df.loc[len(df.index)] = ['*  = Known to synthesize', names.get(comp), '', '', '']
-    #     df.loc[len(df.index)] = ['** = Known to synthesize', 'compound(s) similar to', names.get(comp), '', '']
-    # df = df[1:]
     return df
 
 
@@ -160,7 +156,7 @@ def mk_blank_list(num):
 
 
 def vennd(df):
-    try: os.mkdir('venn')
+    try: os.mkdir('pvenn')
     except (FileExistsError, OSError) as e: pass
 
     left = '10'
@@ -192,7 +188,7 @@ def vennd(df):
                      xytext=(70, -70), ha='center', textcoords='offset points',
                      bbox=dict(boxstyle='round,pad=0.5', fc='gray', alpha=0.1),
                      arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=-0.5', color='gray'))
-        plt.savefig('venn/' + comp + '.png')
+        plt.savefig('pvenn/' + comp + '.png')
 
 
 if __name__ == '__main__':
