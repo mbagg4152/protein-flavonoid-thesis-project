892f3d70871bb8b1ae8edd047c0d137eb2621631
Maggie <50422221+mbagg4152@users.noreply.github.com>
2022-02-23 15:31:25 -0500

    adding documents

--- projects/mh/parsers/knap_parse.py
+++ projects/mh/parsers/knap_parse.py
@@ -6,8 +6,10 @@ from paconstants import *
 new_dir_plants = '..' + SEP + 'misc_files' + SEP + 'knapsack-plants'
 comp_dir = '..' + SEP + 'misc_files' + SEP + 'knapsack-comps'
 ks_plants_file = new_dir_plants + SEP + 'processed_plants.tsv'
+ks_plant_all_comps_file = new_dir_plants + SEP + 'plants_all_comps.tsv'
 ks_chem_file = comp_dir + SEP + 'processed_compounds.tsv'
 plant_flavs = {}
+all_plant_comps = {}
 flav_orgs = {}
 PLANT_PARSE = True
 
@@ -16,12 +18,12 @@ def main():
     except FileExistsError: pass
     try: os.mkdir(new_dir_plants + SEP + 'raw-html')
     except FileExistsError: pass
-    
+
     try: os.mkdir(comp_dir)
     except FileExistsError: pass
     try: os.mkdir(comp_dir + SEP + 'raw-html')
     except FileExistsError: pass
-    
+
     # do_chem_parsing()
     do_plant_parsing()
 
@@ -41,14 +43,14 @@ def do_chem_parsing():
             tmp_fname = comp_dir + SEP + 'raw-html' + SEP + tmp_name + '.txt'
             get_knap_file(tmp_fname, tmp_url)
             parse_chem_file(tmp_fname, tmp_name)
-        
+
         ks_str = ''
-        
+
         for key in flav_orgs:
             if len(flav_orgs[key]) > 0:
                 for item in flav_orgs[key]:
                     ks_str += item + '\t' + key + '\n'
-        
+
         f.write(ks_str)  # write the formatted string to the file
 
 def parse_chem_file(file_name, chem_name):
@@ -62,35 +64,46 @@ def parse_chem_file(file_name, chem_name):
 
 def do_plant_parsing():
     global plant_flavs
-    
+
     for key in plant_dict:
         tmp_val = plant_dict[key].split()[0] + ' ' + plant_dict[key].split()[1]
-        
+
         # plant_flavs[tmp_val] = []
         tmp_url = URL_KNAP_ORG + tmp_val  # fill out URL for the plant
         tmp_url = tmp_url.replace(' ', '%20')
         tmp_file_name = new_dir_plants + SEP + 'raw-html' + SEP + key + ".txt"
         get_knap_file(tmp_file_name, tmp_url)  # download the page for the plant
         parse_plant_file(tmp_file_name, tmp_val)  # parse the downloaded file
-    
+
     with open(ks_plants_file, 'w') as f:
         ks_str = ''
         for key in plant_flavs:
             if len(plant_flavs[key]) > 0:  # at least one entry was found for the plant
                 ks_str += key + '\t' + '\t'.join(plant_flavs.get(key)) + '\n'
             else: ks_str += key + '\n'
-        
+
+        f.write(ks_str)  # write the formatted string to the file
+    with open(ks_plant_all_comps_file, 'w') as f:
+        ks_str = ''
+        for key in all_plant_comps:
+            if len(all_plant_comps[key]) > 0:  # at least one entry was found for the plant
+                all_plant_comps[key].sort()
+                for item in all_plant_comps.get(key):
+                    ks_str += key + '\t' + item + '\n'
+                # ks_str += key + '\t' + '\t'.join(all_plant_comps.get(key)) + '\n'
+            else: ks_str += key + '\n'
+
         f.write(ks_str)  # write the formatted string to the file
 
 def parse_plant_file(file_name, plant_name):
-    global plant_flavs
+    global plant_flavs, all_plant_comps
     with open(file_name, 'r') as file:
         lines = file.readlines()  # each metabolite line begins with this HTML tag
-        
+
         for line in lines:
             has_cas = re.findall(RE_HAS_CAS, line)
             no_cas = re.findall(RE_NO_CAS, line)
-            
+
             if len(has_cas) > 0:
                 tmp_id = has_cas[0][0]
                 tmp_name = has_cas[0][2]
@@ -100,8 +113,12 @@ def parse_plant_file(file_name, plant_name):
                 tmp_name = no_cas[0][1]
                 knap_org_name = (no_cas[0][2] + '' + no_cas[0][3] + ' ' + no_cas[0][4]).strip()
             else: continue
+
             if knap_org_name not in plant_flavs.keys(): plant_flavs[knap_org_name] = []
+            if knap_org_name not in all_plant_comps.keys(): all_plant_comps[knap_org_name] = []
             for key in knap_flav_dict:
+                if tmp_name not in all_plant_comps.get(knap_org_name):
+                    all_plant_comps[knap_org_name].append(tmp_name)
                 if key.strip() in tmp_id.strip():
                     plant_flavs[knap_org_name].append(knap_flav_dict.get(key))
 
